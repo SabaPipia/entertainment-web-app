ef4c8feb1bf8650ed729dab72abce2629aa1c89d
Saba <pipiasaba788@gmail.com>
2023-09-03 19:26:52 +0400

    implemented dynamic changes in input placeholders and headers; fixed bookmarked shows filtering

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -29,21 +29,39 @@ function Home() {
       .catch((err) => {});
   };
   const carouselItems = data?.filter((item: DataTypes) => item.isTrending);
+
   let recomendedItems: DataTypes[] | undefined;
+  let BookmarkedMovies: DataTypes[] | undefined;
+  let BookmarkedTvSeries: DataTypes[] | undefined;
+  let heading = "Recomended for you";
+
   if (pathname === "/home") {
+    heading = "Recomended for you";
     recomendedItems = data?.filter((item: DataTypes) => !item.isTrending);
   } else if (pathname === "/movies") {
+    heading = "Movies";
     recomendedItems = data?.filter(
       (item: DataTypes) => item.category === "Movie"
     );
   } else if (pathname === "/tv-series") {
+    heading = "TV Series";
     recomendedItems = data?.filter(
       (item: DataTypes) => item.category === "TV Series"
     );
   } else if (pathname === "/bookmarks") {
-    recomendedItems = data?.filter((item: DataTypes) => item.isBookmarked);
+    BookmarkedMovies = data?.filter((item: DataTypes) => {
+      if (item.isBookmarked && item.category === "Movie") {
+        return item;
+      }
+    });
+    BookmarkedTvSeries = data?.filter((item: DataTypes) => {
+      if (item.isBookmarked && item.category === "TV Series") {
+        return item;
+      }
+    });
   }
 
+  console.log(BookmarkedMovies);
   return (
     <>
       <SearchBar />
@@ -57,15 +75,23 @@ function Home() {
           )}
         </div>
       ) : null}
-
-      <>
-        <h2 className="text-2xl text-white my-3">Recomended for you</h2>
-        {recomendedItems ? (
-          <Card item={recomendedItems} />
-        ) : (
-          <p>Loading or empty data...</p>
-        )}
-      </>
+      {pathname === "/bookmarks" ? (
+        <>
+          <h2 className="text-2xl text-white my-3">Bookmarked Movies</h2>
+          {BookmarkedMovies ? <Card item={BookmarkedMovies} /> : null}
+          <h2 className="text-2xl text-white my-3">Bookmarked TV Series</h2>
+          {BookmarkedTvSeries ? <Card item={BookmarkedTvSeries} /> : null}
+        </>
+      ) : (
+        <>
+          <h2 className="text-2xl text-white my-3">{heading}</h2>
+          {recomendedItems ? (
+            <Card item={recomendedItems} />
+          ) : (
+            <p>Loading or empty data...</p>
+          )}
+        </>
+      )}
     </>
   );
 }
--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { usePathname } from "next/navigation";
 
 function SearchBar() {
+  const pathname = usePathname();
+  let inputEnding = "movies or TV series";
+  switch (pathname) {
+    case "/movies":
+      inputEnding = "movies";
+      break;
+    case "/tv-series":
+      inputEnding = "TV series";
+      break;
+    case "/bookmarks":
+      inputEnding = "bookmarked shows";
+      break;
+    default:
+      break;
+  }
+
   return (
     <input
       className="w-full bg-transparent p-5 text-white text-lg focus:outline-none md:pt-0 md:pl-2"
-      placeholder="Search for movies or TV series"
+      placeholder={`Search for ${inputEnding}`}
     />
   );
 }
