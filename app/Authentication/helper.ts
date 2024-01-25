f542ccd8128eab5736d9c46322ed50b19cab08fa
saba <pipiasaba788@gmail.com>
2024-01-25 14:52:42 +0400

    improve login

--- app/Authentication/helper.ts
+++ app/Authentication/helper.ts
@@ -1,6 +1,11 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { Dispatch, FormEvent, SetStateAction } from "react";
 import { auth } from "@/firebase/firebase";
+import { setUser } from "@/store/authActions";
+import { useDispatch } from "react-redux";
 
 export const signUp = ({
   e,
@@ -36,6 +41,51 @@ export const signUp = ({
       }
       if (password != rePassword) {
         setError("Passwords do not match");
+      }
+    });
+};
+
+type AppDispatch = Dispatch<any>;
+
+type Router = {
+  push: (url: string) => void;
+  // Add other router methods you use if necessary
+};
+
+export const signIn = ({
+  e,
+  email,
+  password,
+  setError,
+  dispatch,
+  router,
+}: {
+  e: FormEvent;
+  email: string;
+  password: string;
+  setError: Dispatch<SetStateAction<string>>;
+  dispatch: AppDispatch;
+  router: Router;
+}) => {
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      dispatch(setUser(userCredential));
+      router.push("/home");
+    })
+    .catch((err) => {
+      switch ([err][0].code) {
+        case "auth/user-not-found":
+          setError("Email address not registered.");
+          break;
+        case "auth/invalid-email":
+          setError("Enter a valid email.");
+          break;
+        case "auth/wrong-password":
+          setError("Incorrect password, try again");
+          break;
+        default:
+          break;
       }
     });
 };
--- app/Authentication/login/page.tsx
+++ app/Authentication/login/page.tsx
@@ -1,71 +1,74 @@
 "use client";
 
-import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/firebase/firebase";
+import React, { ChangeEvent, FormEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
-import { setUser } from "@/store/authActions";
 
 import Image from "next/image";
 import logo from "@/public/assets/logo.svg";
 import InputBar from "@/components/InputBar";
 import CustomButton from "@/components/CustomButton";
 import Link from "next/link";
+import { Dispatch } from "redux";
+import { signIn } from "../helper";
+import Header from "@/components/ui/Auth/Header";
+import ErrorText from "@/components/ui/Auth/ErrorText";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const dispatch = useDispatch();
-
+  const dispatch: Dispatch = useDispatch();
   const router = useRouter();
-  const signIn = (e: any) => {
+
+  const inputs = [
+    {
+      value: email,
+      name: "email",
+      placeHolder: "Email adress",
+      onChange: (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value),
+    },
+    {
+      value: password,
+      name: "password",
+      placeHolder: "Password",
+      onChange: (e: ChangeEvent<HTMLInputElement>) =>
+        setPassword(e.target.value),
+    },
+  ];
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        dispatch(setUser(userCredential));
-        router.push("/home");
-      })
-      .catch((err) => {
-        switch ([err][0].code) {
-          case "auth/user-not-found":
-            setError("Email address not registered.");
-            break;
-          case "auth/invalid-email":
-            setError("Enter a valid email.");
-            break;
-          case "auth/wrong-password":
-            setError("Incorrect password, try again");
-            break;
-          default:
-            break;
-        }
-      });
+    signIn({
+      e,
+      email,
+      password,
+      setError,
+      dispatch,
+      router,
+    });
   };
+
   return (
     <div className="flex flex-col gap-20 w-full h-full justify-center items-center">
       <Image src={logo} alt="logo" />
       <div className="text-white bg-semiDarkBlue p-8 flex flex-col gap-6 w-80 rounded-lg md:w-96">
-        <h3 className="text-3xl">Login</h3>
-        <form className="w-full flex flex-col gap-5" onSubmit={signIn}>
-          <InputBar
-            value={email}
-            name="email"
-            type="email"
-            placeHolder="Email adress"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <InputBar
-            value={password}
-            name="password"
-            type="password"
-            placeHolder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <Header title="Login" />
+        <form className="w-full flex flex-col gap-5" onSubmit={handleSubmit}>
+          {inputs.map((input) => {
+            return (
+              <InputBar
+                value={input.value}
+                name={input.name}
+                type={input.name}
+                placeHolder={input.placeHolder}
+                onChange={input.onChange}
+              />
+            );
+          })}
           <CustomButton type="submit">Login to your account</CustomButton>
-          <p className="text-customRed text-xs text-center">{error}</p>
+          <ErrorText complete={""} error={error} />
         </form>
         <p className="text-sm text-center">
           Don't have an account?{" "}
