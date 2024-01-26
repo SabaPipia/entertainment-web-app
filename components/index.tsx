4366544dc2427329af485dca17d9359d803858d9
saba <pipiasaba788@gmail.com>
2024-01-26 15:42:16 +0400

    refactor code

--- components/index.tsx
+++ components/index.tsx
@@ -2,7 +2,7 @@ import CustomButton from "./ui/Auth/SubmitButton";
 import InputBar from "./InputBar";
 import Navigation from "./Navigation";
 import SearchBar from "./SearchBar";
-import Card from "./Card";
+import Card from "./ui/Card";
 import Carousel from "./Carousel";
 
 export { CustomButton, InputBar, Navigation, SearchBar, Card, Carousel };
